9cb1d2122309844429297054273b93b006b47bde
Raffay6671 <rafay.abbasi6671@gmail.com>
2024-10-31 21:10:36 +0500

    Passenger App Done using the concepts of JS

--- index.js
+++ index.js
@@ -52,15 +52,53 @@
 // }
 
 // now simple counter App working functionality along with save functionality
-let getCounter = document.getElementById("count-el"); //Passing the arguments
-console.log(getCounter);
+// let getCounter = document.getElementById("count-el"); //Passing the arguments
+// console.log(getCounter);
+// let count = 0;
+// let log = 0;
+// function increment() {
+//   count = count + 1;
+//   getCounter.innerText = count;
+// }
+// function save() {
+//   log = count;
+//   console.log(log);
+// }
+
+//Now testing the strings functionality and concatenation concept
+// let test = "You have tree new notifictaios";
+// console.log(test);
+// let username = "Raffay";
+// let message = "You have three new notifications";
+
+// let messageToUser = message + " , " + username + "! ";
+// console.log(messageToUser);
+
+// let name = "Raffay";
+// let greeting = "Hi, my name is";
+// let myGreeting = greeting + " " + name;
+// console.log(myGreeting);
+
+//A bigger activity of revising the above concepts
+// let welcomeEl = document.getElementById("welcome-el");
+// console.log(welcomeEl);
+// let name = "Raffay";
+// let greeting = "Welcome Back per ";
+// welcomeEl.innerText = greeting + name;
+// welcomeEl.innerText += "👋";
+
+//Now implementing the Save Functionality with Previous Feautures as well-difference between innerText and textContent
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
 let count = 0;
-let log = 0;
 function increment() {
-  count = count + 1;
-  getCounter.innerText = count;
+  count += 1;
+  countEl.innerText = count;
 }
 function save() {
-  log = count;
-  console.log(log);
+  combine = count + " - ";
+
+  saveEl.textContent += combine;
+  countEl.textContent = 0;
+  count = 0;
 }
